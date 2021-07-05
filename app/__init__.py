52b9c0022cef7752d7ef9e31059565abe3bc7b0c
Kalidou SY <kalidou.harouna.sy@gmail.com>
2021-07-05 21:00:50 +0000

    ksy

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template 
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from config import config
+
+login_manager = LoginManager()
+login_manager.login_view = 'main.login'
 
 db = SQLAlchemy()
 
@@ -8,6 +12,7 @@ def create_app(config_name):
     app = Flask(__name__) 
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
+    #injections de dépendance
     db.init_app(app)
     # attach routes and custom error pages here
 
@@ -19,5 +24,7 @@ def create_app(config_name):
 
     from .admin import admin as admin_blueprint 
     app.register_blueprint(admin_blueprint)
+
+    login_manager.init_app(app) 
 
     return app
